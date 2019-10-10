a2ce79ef922a40641b6ec22a84e04faccd7e9ec2
Lexa <lexaraea@gmail.com>
2019-10-10 16:49:35 -0400

    First pages + basic endpoints working + basic user login working

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import { withRouter } from "react-router";
 import { render } from 'react-dom';
 
 import {
@@ -6,6 +7,8 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+
+import { withRouter } from 'react-router';
 
 class SigninForm extends Component {
   render() {
@@ -15,22 +18,137 @@ class SigninForm extends Component {
   }
 }
 
-class SignupForm extends Component {
+class UserProfile extends Component {
   render() {
+    const { user } = this.props;
+
     return (
       <div>
-        <h2>Sign up form</h2>
-
-        <input type="text" placeholder="Your name" />
-        <input type="email" placeholder="Your email"/>
-        <input type="password" placeholder="Your password" />
+        <h2>User Profile</h2>
+        <span>{ user.name }</span>
       </div>
     );
   }
 }
 
-class App extends Component {
+class SignupForm extends Component {
   render() {
+    const {
+      state: {
+        name,
+        email,
+        password,
+      },
+      onNameUpdate,
+      onEmailUpdate,
+      onPasswordUpdate,
+      onSubmit,
+
+      history,
+    } = this.props;
+
+    return (
+      <div>
+        <h2>Sign up form</h2>
+
+        <div>
+          <input
+            type="text"
+            onChange={ e => onNameUpdate(e.target.value) }
+            value={ name }
+            placeholder="Your name" />
+        </div>
+        <div>
+          <input
+            type="email"
+            onChange={ e => onEmailUpdate(e.target.value) }
+            value={ email }
+            placeholder="Your email" />
+        </div>
+        <div>
+          <input
+            type="password"
+            onChange={ e => onPasswordUpdate(e.target.value) }
+            value={ password }
+            placeholder="Your password" />
+        </div>
+        <div>
+          <button type="button" onClick={ () => {
+            onSubmit();
+            history.push('/app/user/profile');
+          }}>Continue</button>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+const SignupFormWithRouter = withRouter(SignupForm);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+      signUpForm: {
+        name: '',
+        email: '',
+        password: '',
+      },
+    };
+  }
+
+  onNameUpdate(name) {
+    const { signUpForm } = this.state;
+
+    const updatedForm = Object.assign({}, signUpForm, { name });
+
+    this.setState({
+      signUpForm: updatedForm,
+    });
+  }
+
+  onEmailUpdate(email) {
+    const { signUpForm } = this.state;
+
+    const updatedForm = Object.assign({}, signUpForm, { email });
+
+    this.setState({
+      signUpForm: updatedForm,
+    });
+  }
+
+  onPasswordUpdate(password) {
+    const { signUpForm } = this.state;
+
+    const updatedForm = Object.assign({}, signUpForm, { password });
+
+    this.setState({
+      signUpForm: updatedForm,
+    });
+  }
+
+  onSignUpSubmit() {
+    const { signUpForm } = this.state;
+
+    this.setState({
+      currentUser: {
+        name: signUpForm.name,
+        email: signUpForm.email,
+      },
+      signUpForm: {
+        name: '',
+        email: '',
+        password: '',
+      },
+    });
+  }
+
+  render() {
+    const { currentUser, signUpForm } = this.state;
+
     return (
       <Router>
         <div>
@@ -39,8 +157,19 @@ class App extends Component {
             <li><Link to="/app/signup">Sign up</Link></li>
           </ul>
           <div>
+            <Route path="/app/signup" render={ () => (
+              <SignupFormWithRouter
+                state={ signUpForm }
+                onNameUpdate={ this.onNameUpdate.bind(this) }
+                onEmailUpdate={ this.onEmailUpdate.bind(this) }
+                onPasswordUpdate={ this.onPasswordUpdate.bind(this) }
+                onSubmit={ this.onSignUpSubmit.bind(this) }
+                />
+            )} />
             <Route path="/app/signin" component={ SigninForm } />
-            <Route path="/app/signup" component={ SignupForm } />
+            <Route path="/app/user/profile" render={ () => (
+              <UserProfile user={ currentUser } />
+            )} />
           </div>
         </div>
       </Router>
